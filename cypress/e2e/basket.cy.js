00ee7b4d5c1aff7955086ab341a0ec88b7a688ae
Dominika N <dominikaniem@gmail.com>
2023-05-06 17:00:34 +0200

    cart test fixes

--- cypress/e2e/basket.cy.js
+++ cypress/e2e/basket.cy.js
@@ -1,12 +1,16 @@
 import HomePage from "../page-objects/homePage";
-import AccountPage from "../page-objects/myAccountPage";
 import CartPage from "../page-objects/cartPage";
-import  { faker } from '@faker-js/faker';
 
 describe('it should test basket', () => {
     const homePage = new HomePage();
-    const accountPage = new AccountPage();
-    const basketPage = new BasketPage();
+    const cartPage = new CartPage();
 
-
+it('should add product to cart and delete from there' , function(){
+    homePage.visitPage(),
+    homePage.addProductToCart(),
+    homePage.clickGoToCartFromProductButton(),
+    cartPage.checkThatAddedProductIsInCart(),
+    cartPage.removeItemFromCart(),
+    cartPage.checkThatCartIsEmpty()
+    })
 })
--- cypress/page-objects/homePage.js
+++ cypress/page-objects/homePage.js
@@ -1,30 +1,28 @@
 import Urls from "./urls";
 const myAccountHeaderButton = '#menu-item-100'
 const dataBlockNameNew = 'div[data-block-name="woocommerce/product-new"]'
-const checkCartFromProductLevel = '.added_to_cart .wc-forward'
+const checkCartFromProductLevel = '.added_to_cart.wc-forward'
 
-const Product = { 
+export const Product = { 
     HoodieWithZipper: {
-    Locator: 'a[data-product_id="53]',
+    Locator: 'a[data-product_id="51"]',
     Name: 'Hoodie with Zipper'
     },
 
     Sunglasess: {
-    Locator:'a[data-product_id="49]',
+    Locator:'a[data-product_id="49"]',
     Name: 'Sunglasses' 
     },
     
     Polo: {
-    Locator: 'a[data-product_id="53]',
+    Locator: 'a[data-product_id="53"]',
     Name: 'Polo'
     },
     
 }
 
-
-
-
 class HomePage{
+
     clickMyAccountHeaderButton() {
         cy.get(myAccountHeaderButton).click()
     } 
@@ -36,15 +34,13 @@ class HomePage{
 
     addProductToCart(){
         cy.get(dataBlockNameNew).within(()=>{
-            cy.get(Product.HoodieWithZipper.Locator)
+            cy.get(Product.HoodieWithZipper.Locator).click()
         })
     }
     clickGoToCartFromProductButton(){
         cy.get(dataBlockNameNew).within(()=>{
         cy.get(checkCartFromProductLevel).click()
     })
-}
+}}
 
 export default HomePage;
-
-    
